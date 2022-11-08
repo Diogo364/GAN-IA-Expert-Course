71a8dfc0189ce41bc5d77e4cce3d641f9c133781
Diogo364 <diogotnascimento94@gmail.com>
2022-11-08 09:20:02 -0300

    New: Environment variables

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,11 @@
 import numpy as np
+from dotenv import load_dotenv
 
 def normalize(data):
     flatten_data = np.ravel(data)
     max_value, min_value = flatten_data.max(), flatten_data.min()
     middle = (max_value + min_value) / 2
     return (data - middle) / middle
+
+def load_env():
+    load_dotenv('.env')
