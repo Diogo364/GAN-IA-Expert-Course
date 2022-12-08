d60ee0e86b6dae6094170a865f5fd04a55975bbc
Diogo364 <diogotnascimento94@gmail.com>
2022-12-08 15:08:57 -0300

    New: Pix2Pix GAN Pipeline

--- models/nn/gan_pipeline.py
+++ models/nn/gan_pipeline.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 from utils.custom_losses import GradientPenaltyLossInterface
 from .baseInterfaces import AbstractGANPipeline, AbstractDLModel
 import numpy as np
@@ -66,3 +67,44 @@ class WGAN_GPPipeline(AbstractGANPipeline):
         gradient = tape.gradient(scores, interpolated_images)[0]
         gradient_norm = tf.norm(gradient)
         return tf.math.reduce_mean((gradient_norm -1)**2)
+
+class Pix2PixPipeline(AbstractGANPipeline):
+    def __init__(self, 
+                generator: AbstractDLModel, 
+                discriminator: AbstractDLModel, 
+                generator_loss: tf.keras.losses.Loss, 
+                p2p_loss: tf.keras.losses.Loss, 
+                discriminator_loss: GradientPenaltyLossInterface,
+                lambda_: float):
+        super().__init__(generator, discriminator, generator_loss, discriminator_loss)
+        self._p2p_loss = p2p_loss
+        self._lambda = lambda_
+
+    @tf.function
+    def train(self, images: Tuple):
+        raw_images, processed_images = images
+        with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+            generated_images = self._generator.predict(raw_images, training=True)
+            expected_output = self._discriminator.predict([raw_images, processed_images], training=True)
+            fake_output = self._discriminator.predict([raw_images, generated_images], training=True)
+            
+            gen_loss = self._apply_generator_loss(expected_output)
+            pix2pix_loss = self._apply_pix2pix_loss(generated_images, processed_images)
+            total_gen_loss = self._join_generator_losses(gen_loss, pix2pix_loss)
+            
+            disc_loss = self._discriminator_loss(expected_output, fake_output)
+        
+        self._generator.update_weights(gen_tape, total_gen_loss)
+        self._discriminator.update_weights(disc_tape, disc_loss)
+
+    def _apply_discriminator_loss(self, expected_output, fake_output):
+        return super()._apply_discriminator_loss(expected_output, fake_output)
+
+    def _apply_generator_loss(self, fake_output):
+        return super()._apply_generator_loss(fake_output)
+
+    def _apply_pix2pix_loss(self, generator_output, target):
+        return self._p2p_loss(target, generator_output)
+
+    def _join_generator_losses(self, gen_loss, pix2pix_loss):
+        return gen_loss + (self._lambda * pix2pix_loss)
