7a6072d4f0aea4b85623c06761cd334492fa603b
Diogo Nascimento <diogotnascimento94@gmail.com>
2022-12-21 14:12:08 -0400

    New: CycleGAN Apple to Oranges

---
+++ CycleGAN_apples2oranges.py
@@ -0,0 +1,105 @@
+import os
+import os.path as osp
+from tqdm import tqdm
+import tensorflow as tf
+from tensorflow_examples.models.pix2pix import pix2pix
+from utils import normalize, random_crop, random_flip, load_env, tf_normalize
+import matplotlib.pyplot as plt
+from models.dataloaders import ApplesNOrangesDataLoader
+from models.preprocessor import GenericPreprocessor
+from models.nn.gan_pipeline import CycleGANPipeline
+from models.nn.baseInterfaces import CustomArchitectureModel
+from utils.custom_losses import MeanAbsoluteErrorLoss
+
+load_env()
+
+def get_fruit_data():
+    generic_train_preprocessor = GenericPreprocessor(preprocessor_list=[random_crop, random_flip, normalize])
+    generic_test_preprocessor = GenericPreprocessor(preprocessor_list=[normalize])
+    (train, test) = ApplesNOrangesDataLoader().load_data()
+    train = train.cache().map(generic_train_preprocessor)
+    test = test.cache().map(generic_test_preprocessor)
+    return train, test
+
+if __name__ == '__main__':
+    OUTPATH = osp.join(os.environ['OUTPATH'], 'Apple2Orange_CycleGAN')
+    CycleGAN_ASSETS = osp.join(os.environ['ASSETS'], 'Apple2Orange_CycleGAN')
+
+    os.makedirs(OUTPATH, exist_ok=True)
+    os.makedirs(CycleGAN_ASSETS, exist_ok=True)
+
+    test_size = 16
+    batch_size = 1
+    buffer_size = 995
+    random_noise_size = (256,256,3)
+    epochs = 10
+    save_artefact_frequency = 5 # each n epochs
+    learning_rate = 0.0002
+    beta1, beta2 = 0.5, 0.999
+    lambda_ = 10
+
+    train, test = get_fruit_data()
+    
+    train = train.shuffle(buffer_size).batch(batch_size)
+    test = test.batch(batch_size)
+
+    test_images = test.take(test_size)
+
+    generator_A2B_optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate, beta_1=beta1, beta_2=beta2)
+    generator_B2A_optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate, beta_1=beta1, beta_2=beta2)
+    
+    discriminator_A2B_optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate, beta_1=beta1, beta_2=beta2)
+    discriminator_B2A_optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate, beta_1=beta1, beta_2=beta2)
+
+    generator_loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+    discriminator_loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+    cycle_loss = MeanAbsoluteErrorLoss()
+    identity_loss = MeanAbsoluteErrorLoss()
+
+    generator_A2B = CustomArchitectureModel(model=pix2pix.unet_generator(random_noise_size[-1], norm_type='instancenorm'), 
+                                            optimizer=generator_A2B_optimizer,
+                                            input_shape=random_noise_size)
+
+    generator_B2A = CustomArchitectureModel(model=pix2pix.unet_generator(random_noise_size[-1], norm_type='instancenorm'), 
+                                            optimizer=generator_B2A_optimizer,
+                                            input_shape=random_noise_size)
+
+    
+    discriminator_A2B = CustomArchitectureModel(model=pix2pix.discriminator(norm_type='instancenorm', target=False), 
+                                            optimizer=discriminator_A2B_optimizer,
+                                            input_shape=random_noise_size)
+
+    discriminator_B2A = CustomArchitectureModel(model=pix2pix.discriminator(norm_type='instancenorm', target=False), 
+                                            optimizer=discriminator_B2A_optimizer,
+                                            input_shape=random_noise_size)
+
+    pipeline = CycleGANPipeline(generatorA2B=generator_A2B,
+                                generatorB2A=generator_B2A,
+                                discriminatorA2B=discriminator_A2B,
+                                discriminatorB2A=discriminator_B2A,
+                                generator_loss=generator_loss,
+                                discriminator_loss=discriminator_loss,
+                                cycle_loss=cycle_loss,
+                                identity_loss=identity_loss,
+                                lambda_=lambda_)
+
+    for epoch in tqdm(range(epochs), leave=True, desc='Training Epoch'):
+        for (train_A, train_B) in tqdm(train, total=len(list(train)), desc='Batches', leave=False):
+            pipeline.train((train_A, train_B))
+        if (epoch == 0) or ((epoch+1) % save_artefact_frequency == 0):
+            fig, axes = plt.subplots(4, 4, figsize=(20,20), facecolor='white')
+            for (test_A, test_B), ax in zip(test.take(16), axes.ravel()):
+                test_A2B = tf.concat([test_A, generator_A2B.predict(test_A)], axis=2)
+                test_B2A = tf.concat([test_B, generator_B2A.predict(test_B)], axis=2)
+                ax.imshow(
+                    tf_normalize(
+                        tf.concat([test_A2B, test_B2A], axis=1)[0], 
+                        (0, 1)
+                    )
+                )
+                ax.axis('off')
+            fig.savefig(osp.join(OUTPATH, f'epoch_{epoch+1}_out_grid.png'))
+            plt.close(fig)
+
+    pipeline._generator.save_model(osp.join(CycleGAN_ASSETS, 'saved_model_A2B.hdf5'))
+    pipeline._generator_reverse.save_model(osp.join(CycleGAN_ASSETS, 'saved_model_B2A.hdf5'))
--- models/nn/gan_pipeline.py
+++ models/nn/gan_pipeline.py
@@ -74,7 +74,7 @@ class Pix2PixPipeline(AbstractGANPipeline):
                 discriminator: AbstractDLModel, 
                 generator_loss: tf.keras.losses.Loss, 
                 p2p_loss: tf.keras.losses.Loss, 
-                discriminator_loss: GradientPenaltyLossInterface,
+                discriminator_loss: tf.keras.losses.Loss,
                 lambda_: float):
         super().__init__(generator, discriminator, generator_loss, discriminator_loss)
         self._p2p_loss = p2p_loss
@@ -108,3 +108,75 @@ class Pix2PixPipeline(AbstractGANPipeline):
 
     def _join_generator_losses(self, gen_loss, pix2pix_loss):
         return gen_loss + (self._lambda * pix2pix_loss)
+
+class CycleGANPipeline(AbstractGANPipeline):
+    def __init__(self, 
+                generatorA2B: AbstractDLModel, 
+                generatorB2A: AbstractDLModel, 
+                discriminatorA2B: AbstractDLModel, 
+                discriminatorB2A: AbstractDLModel, 
+                generator_loss: tf.keras.losses.Loss, 
+                cycle_loss: tf.keras.losses.Loss, 
+                identity_loss: tf.keras.losses.Loss, 
+                discriminator_loss: tf.keras.losses.Loss,
+                lambda_: float):
+        super().__init__(generatorA2B, discriminatorA2B, generator_loss, discriminator_loss)
+        self._generator_reverse = generatorB2A
+        self._discriminator_reverse = discriminatorB2A
+        self._cycle_loss = cycle_loss
+        self._identity_loss = identity_loss
+        self._lambda = lambda_
+
+    @tf.function
+    def train(self, images: Tuple):
+        image_A, image_B = images
+        with tf.GradientTape(persistent=True) as tape:
+            # A -> B fake
+            generated_B = self._generator.predict(image_A, training=True)
+            # B fake -> A fake
+            cycled_A = self._generator_reverse.predict(generated_B, training=True)
+            # Discriminator A2B
+            expected_output_B = self._discriminator.predict(image_B, training=True)
+            fake_output_B = self._discriminator.predict(generated_B, training=True)
+            
+            # B -> A fake
+            generated_A = self._generator_reverse.predict(image_B, training=True)
+            # A fake -> B fake
+            cycled_B = self._generator.predict(generated_A, training=True)
+            # Discriminator B2A
+            expected_output_A = self._discriminator_reverse.predict(image_A, training=True)
+            fake_output_A = self._discriminator_reverse.predict(generated_A, training=True)
+            
+            # Identity Mapping
+            ## A -> A fake
+            same_A = self._generator_reverse.predict(image_A, training=True)
+            ## B -> B fake
+            same_B = self._generator.predict(image_B, training=True)
+
+            gen_A2B_loss = self._apply_generator_loss(fake_output_B)
+            gen_B2A_loss = self._apply_generator_loss(fake_output_A)
+
+            cycle_loss = self._apply_cycle_loss(cycled_A, image_A) + self._apply_cycle_loss(cycled_B, image_B)
+            total_gen_A2B_loss = gen_A2B_loss + cycle_loss + self._apply_identity_loss(same_B, image_B)
+            total_gen_B2A_loss = gen_B2A_loss + cycle_loss + self._apply_identity_loss(same_A, image_A)
+            
+            disc_A2B_loss = self._discriminator_loss(expected_output_B, fake_output_B)
+            disc_B2B_loss = self._discriminator_loss(expected_output_A, fake_output_A)
+        
+        self._generator.update_weights(tape, total_gen_A2B_loss)
+        self._discriminator.update_weights(tape, disc_A2B_loss)
+        
+        self._generator_reverse.update_weights(tape, total_gen_B2A_loss)
+        self._discriminator_reverse.update_weights(tape, disc_B2B_loss)
+
+    def _apply_discriminator_loss(self, expected_output, fake_output):
+        return super()._apply_discriminator_loss(expected_output, fake_output)
+
+    def _apply_generator_loss(self, fake_output):
+        return super()._apply_generator_loss(fake_output)
+
+    def _apply_cycle_loss(self, generator_output, target):
+        return self._lambda * self._cycle_loss(target, generator_output)
+    
+    def _apply_identity_loss(self, generator_output, target):
+        return self._lambda * 0.5 * self._identity_loss(target, generator_output)
